a0bdc2c9195bdc15dedae734a02718e894465823
Jamie Brandon <jamie@scattered-thoughts.net>
2021-05-04 18:44:33 -0700

    Sketching

--- bin/test.zig
+++ bin/test.zig
@@ -1,6 +1,49 @@
 const std = @import("std");
 const dida = @import("../lib/dida.zig");
 
-pub fn main() void {
+var gpa = std.heap.GeneralPurposeAllocator(.{
+    .safety = true,
+    .never_unmap = true,
+}){};
+const allocator = &gpa.allocator;
+
+pub fn main() !void {
+    defer {
+        _ = gpa.detectLeaks();
+    }
+
+    var graph_builder = dida.GraphBuilder.init(allocator);
+    defer graph_builder.deinit();
+    const foo = try graph_builder.add_node(.Input);
+    const bar = try graph_builder.add_node(.Input);
+    const bar_inc = graph_builder.add_node(.{
+        .Map = .{
+            .input = bar,
+            .function = (struct {
+                fn inc(input: dida.Datum) error{OutOfMemory}!dida.Datum {
+                    var output = try std.mem.dupe(allocator, u8, input);
+                    output[1] += 1;
+                    return output;
+                }
+            }).inc,
+        },
+    });
+    const key = (struct {
+        fn key(input: dida.Datum) dida.Datum {
+            return input[0..1];
+        }
+    }).key;
+    const foobar = try graph_builder.add_node(.{
+        .Join = .{
+            .left_input = foo,
+            .right_input = bar,
+            .left_key_function = key,
+            .right_key_function = key,
+        },
+    });
+
+    const graph = graph_builder.finish_and_clear();
+    defer graph.deinit();
+
     std.debug.print("Hello world!", .{});
 }
--- lib/dida.zig
+++ lib/dida.zig
@@ -2,75 +2,113 @@ usingnamespace @import("dida/common.zig");
 
 pub const meta = @import("dida/meta.zig");
 
-const Node = struct {
+pub const Node = struct {
     id: u64,
 };
 
-const Port = union(enum) {
+pub const Port = union(enum) {
     Input: u64,
     Output: u64,
 };
 
-const Location = struct {
+pub const Location = struct {
     node: Node,
     port: Port,
 };
 
-const Timestamp = struct {
+pub const Timestamp = struct {
     coords: []const u64,
 };
 
-const Pointstamp = struct {
+pub const Pointstamp = struct {
     location: Location,
     timestamp: Timestamp,
 };
 
-const Datum = struct {
-    data: []const u8,
-};
+pub const Datum = []const u8;
 
-const Change = struct {
+pub const Change = struct {
     datum: Datum,
     diff: isize,
     timestamp: Timestamp,
 };
 
-const Trace = struct {
+pub const Trace = struct {
     changes: ArrayList(Change),
 };
 
-const NodeKind = enum {
+pub const NodeData = union(enum) {
     Input,
     Map: Map,
     Join: Join,
+
+    pub const Map = struct {
+        input: Node,
+        function: fn (datum: Datum) error{OutOfMemory}!Datum,
+    };
+
+    pub const Join = struct {
+        left_input: Node,
+        right_input: Node,
+        // TODO arrangements?
+        left_key_function: fn (datum: Datum) Datum,
+        right_key_function: fn (datum: Datum) Datum,
+    };
 };
 
-const NodeData = struct {
-    kind: NodeKind,
-};
-
-const Edge = struct {
+pub const Edge = struct {
     in: Node,
     out: Node,
 };
 
-const GraphBuilder = struct {
+pub const GraphBuilder = struct {
     allocator: *Allocator,
     nodes: ArrayList(NodeData),
+
+    const Self = @This();
+
+    pub fn init(allocator: *Allocator) Self {
+        const nodes = ArrayList(NodeData).init(allocator);
+        return Self{
+            .allocator = allocator,
+            .nodes = nodes,
+        };
+    }
+
+    pub fn deinit(self: Self) void {
+        self.nodes.deinit();
+    }
+
+    pub fn add_node(self: *Self, node_data: NodeData) error{OutOfMemory}!Node {
+        const node = Node{ .id = @intCast(u64, self.nodes.items.len) };
+        try self.nodes.append(node_data);
+        return node;
+    }
+
+    pub fn finish_and_clear(self: *Self) Graph {
+        const nodes = self.nodes.toOwnedSlice();
+        return Graph{
+            .allocator = self.allocator,
+            .nodes = nodes,
+        };
+    }
 };
 
-const Graph = struct {
+pub const Graph = struct {
     allocator: *Allocator,
-    nodes: []NodeData,
-    in_edges: [][]Node,
-    out_edges: [][]Node,
+    nodes: []const NodeData,
+
+    const Self = @This();
+
+    pub fn deinit(self: Self) void {
+        self.allocator.free(self.nodes);
+    }
 };
 
-const Pointstamps = HashMap(Pointstamp, u64);
-const PointstampChanges = HashMap(Pointstamp, i64);
+pub const Pointstamps = HashMap(Pointstamp, u64);
+pub const PointstampChanges = HashMap(Pointstamp, i64);
 
-const WorkerState = struct {
+pub const WorkerState = struct {
     graph: Graph,
-    local_pointstamps: ArrayList(Pointstamp),
-    approx_global_pointstamps: ArrayList(PointStamp), // maybe contains dupes
+    unprocessed_changes: ArrayList(struct { location: Location, change: Change }),
 };
