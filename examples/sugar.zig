83cc6ef3b4fdb8b887b4379c32bcc5a5df462f93
Jamie Brandon <jamie@scattered-thoughts.net>
2021-06-08 18:48:32 -0700

    Add sketch of sugar

---
+++ examples/sugar.zig
@@ -0,0 +1,52 @@
+// TODO this is just speculative atm
+
+const std = @import("std");
+const dida = @import("../core/dida.zig");
+
+var gpa = std.heap.GeneralPurposeAllocator(.{
+    .safety = true,
+    .never_unmap = true,
+}){};
+var arena = std.heap.ArenaAllocator.init(&gpa.allocator);
+const allocator = &arena.allocator;
+
+pub fn main() !void {
+    defer {
+        arena.deinit();
+        _ = gpa.detectLeaks();
+    }
+
+    const sugar = dida.sugar.Sugar.init(allocator);
+    const edges = sugar.input();
+    const fixpoint = sugar.fixpoint(.{ .reach = edges });
+    const reach = fixpoint.reach
+        .index()
+        .distinct()
+        .join(edges.project(.{ 1, 0 }).index(), 1)
+        .project(.{ 3, 1 });
+    fixpoint.finish(.{ .reach = reach });
+    const out = fixpoint.reach.output();
+
+    sugar.build();
+
+    try edges.push(.{ .{ "a", "b" }, 1, .{0} });
+    try edges.push(.{ .{ "b", "c" }, 1, .{0} });
+    try edges.push(.{ .{ "c", "d" }, 1, .{0} });
+    try edges.push(.{ .{ "c", "a" }, 1, .{0} });
+    try edges.push(.{ .{ "b", "c" }, -1, .{1} });
+    try edges.flush();
+
+    try edges.advance(.{1});
+    try sugar.doAllWork();
+    while (out.pop()) |change_batch| {
+        dida.common.dump(change_batch);
+    }
+
+    std.debug.print("Advancing!\n", .{});
+
+    try edges.advance(.{2});
+    try sugar.doAllWork();
+    while (out.pop()) |change_batch| {
+        dida.common.dump(change_batch);
+    }
+}
