0ed26ffa2c1e1d7a1f98df8e2770d7d2bad0de6d
Jamie Brandon <jamie@scattered-thoughts.net>
2021-12-27 15:56:37 -0800

    Bump to nixos-21.11

--- dependencies.nix
+++ dependencies.nix
@@ -1,8 +1,8 @@
 rec {
     pkgs = import (builtins.fetchTarball {
-        name = "nixos-20.09";
-        url = "https://github.com/NixOS/nixpkgs/archive/20.09.tar.gz";
-        sha256 = "1wg61h4gndm3vcprdcg7rc4s1v3jkm5xd7lw8r2f67w502y94gcy";
+        name = "nixos-21.11";
+        url = "https://github.com/NixOS/nixpkgs/archive/21.11.tar.gz";
+        sha256 = "162dywda2dvfj1248afxc45kcrg83appjd0nmdb541hl7rnncf02";
     }) {};
 
     zig = pkgs.stdenv.mkDerivation {
