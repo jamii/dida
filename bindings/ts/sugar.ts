f7afb0b03dae45d63033d5e7d4624aec5f0ee163
Marco Munizaga <git@marcopolo.io>
2022-03-28 09:10:29 -0700

    Continue TS sugar

--- bindings/ts/sugar.ts
+++ bindings/ts/sugar.ts
@@ -1,28 +1,36 @@
-import Dida from '../wasm/zig-out/lib/types/dida'
+import Dida from '../wasm/zig-out/lib/types/dida.d.mts'
 
-
-// type Dida = {
-//   GraphBuilder: DidaBuilderClass
-// } & any
-type DidaBuilder = any
-type DidaSubgraph = any
-type DidaNode = any
-type DidaBuilderClass = {
-  new(): DidaBuilder
-}
+type DidaBuilder = InstanceType<Dida["GraphBuilder"]>
 
 type SugarState =
-  { state: "building", builder: DidaBuilder, rootSubGraph: Subgraph }
-  | { state: "running" }
+  { state: "building", builder: InstanceType<Dida["GraphBuilder"]>, rootSubGraph: Subgraph }
+  | { state: "running", shard: InstanceType<Dida["Shard"]> }
+
+type UglyHackToPreserverType<A, T extends Array<any>, G extends Array<any>> =
+  A extends IndexedNode<T> ? IndexedNode<G> :
+  A extends Node<T> ? Node<G> : A
+
+
+type Any1<T> = any
+
 
 export class Sugar {
   state: SugarState
   dida: Dida
 
   constructor(dida: Dida) {
+    const rootID = 0
     const builder = new dida.GraphBuilder()
-    this.state = { state: "building", builder: new dida.GraphBuilder(), rootSubGraph: builder }
+    const rootSubGraph = builder.addSubgraph(rootID)
+    this.state = { state: "building", builder: builder, rootSubGraph }
     this.dida = dida
+  }
+
+  get shard(): InstanceType<Dida["Shard"]> {
+    if (this.state.state !== "running") {
+      throw new Error("Sugar is not in building state")
+    }
+    return this.state.shard
   }
 
   get builder(): DidaBuilder {
@@ -40,15 +48,12 @@ export class Sugar {
   }
 
 
-  input(): InputNode {
-    const builder = this.builder()
-    const rootGraph = this.rootSubGraph()
+  input<T extends Array<any>>(): InputNode<T> {
+    const builder = this.builder
+    const rootGraph = this.rootSubGraph
 
     const node = builder.addNode(rootGraph, new this.dida.NodeSpec.Input());
-    const edges = graph_builder.addNode(subgraph_0, new dida.NodeSpec.Input());
-
-    const edges = this.s.addNode(subgraph_0, new dida.NodeSpec.Input());
-    return new InputNode(this, null)
+    return new InputNode<T>(this, node, rootGraph)
   }
 
   loop(): Subgraph {
@@ -56,107 +61,223 @@ export class Sugar {
       throw new Error("Cannot loop on a graph that is not being built")
     }
 
-    this.state.builder.add
     const subgraph_1 = this.state.builder.addSubgraph(this.state.rootSubGraph);
-    return new Subgraph(this, subgraph_1)
+    return new Subgraph(this, this.rootSubGraph, subgraph_1)
   }
 
-  build() { }
-  doAllWork() { }
+  build() {
+    const graph = this.builder.finishAndReset();
+    this.state = { state: "running", shard: new this.dida.Shard(graph) }
+  }
+
+  doAllWork() {
+    while (this.shard.hasWork()) {
+      this.shard.doWork()
+    }
+  }
 }
 
 export class Subgraph {
   readonly sugar: Sugar
-  readonly inner: DidaSubgraph
+  readonly inner: InstanceType<Dida["Subgraph"]>
+  readonly parent: Subgraph
 
-  constructor(sugar: Sugar, inner: DidaSubgraph) {
+  constructor(sugar: Sugar, parent: Subgraph["parent"], inner: Subgraph["inner"]) {
     this.sugar = sugar
     this.inner = inner
+    this.parent = parent
   }
 
   // loop(): Subgraph {
   //   return new Subgraph()
   // }
 
-  // loopNode(): Node {
-  //   return new Node()
-  // }
+  loopNode<T extends Array<any>>(): TimestampIncrementedNode<T> {
+    const builder = this.sugar.builder
 
-  importNode(n: Node): Node {
-    if (this.sugar.state.state !== "building") {
-      throw new Error("Not in building state")
-    }
+    const nodeInner = builder.addNode(this.inner, new this.sugar.dida.NodeSpec.TimestampIncrement())
 
+    return new TimestampIncrementedNode(this.sugar, nodeInner, this)
+  }
+
+  importNode<T extends Array<any>>(n: Node<T>): TimestampPushedNode<T> {
+    const builder = this.sugar.builder
     // TODO add check for
     // "Can only import from parent subgraph into child subgraph",
-    console.log("asdf")
-    console.log(this.inner)
-    console.log(this.inner.id)
 
-    return new Node(this.sugar, this.inner)
+    const nodeInner = builder.addNode(
+      this.inner,
+      new this.sugar.dida.NodeSpec.TimestampPush(n.inner)
+    )
+
+    return new TimestampPushedNode(this.sugar, nodeInner, this)
   }
 
-  // exportNode(n: Node): Node {
-  //   return new Node()
-  // }
+  exportNode<T extends Array<any>>(n: Node<T>): TimestampPoppedNode<T> {
+    const builder = this.sugar.builder
+    // TODO add check for
+    // "Can only export from child subgraph into parent subgraph",
+
+    const nodeInner = builder.addNode(
+      this.parent.inner,
+      new this.sugar.dida.NodeSpec.TimestampPush(n.inner)
+    )
+
+    return new TimestampPushedNode(this.sugar, nodeInner, this.parent)
+  }
 }
 
-export class Node {
+export class Node<T extends Array<any>> {
   readonly sugar: Sugar
-  readonly inner: DidaNode
+  readonly inner: ReturnType<DidaBuilder["addNode"]>
+  readonly subgraph: Subgraph
 
-  constructor(sugar: Sugar, inner: DidaNode) {
+  constructor(sugar: Sugar, inner: Node<T>["inner"], subgraph: Node<T>["subgraph"]) {
     this.sugar = sugar
     this.inner = inner
+    this.subgraph = subgraph
   }
 
-  // index(): Node {
-  //   return new Node(this.sugar)
+  index(): IndexedNode<T> {
+    const nodeInner = this.sugar.builder.addNode(
+      this.subgraph,
+      new this.sugar.dida.NodeSpec.Index(this.inner)
+    )
+
+    return new IndexedNode(this.sugar, nodeInner, this.subgraph)
+  }
+
+  mapInner<G extends Array<any>>(f: (input: T) => G): Node<G> {
+    const nodeInner = this.sugar.builder.addNode(
+      this.subgraph,
+      new this.sugar.dida.NodeSpec.Map(this.inner, f),
+    );
+
+    return new Node<G>(this.sugar, nodeInner, this.subgraph)
+  }
+
+  // Each subclass needs to implement this if they want to map into their own type
+  map<G extends Array<any>>(f: (input: T) => G): Node<G> {
+    return this.mapInner(f)
+  }
+
+  // reduce(): this {
+  //   return this
   // }
 
-  map(f: any): this {
-    return this
-  }
-  reduce(): this {
-    return this
-  }
-  project(): this {
-    return this
-  }
-  union(): this {
-    return this
+  reduce<V>(keyColumns: number, reduceFn: (v: V, row: T, count: number) => V): Node<T> {
+    const nodeInner = this.sugar.builder.addNode(
+      this.subgraph,
+      new this.sugar.dida.NodeSpec.Reduce(
+        this.inner,
+        keyColumns,
+        reduceFn
+      ),
+    );
+
+    return new Node<T>(this.sugar, nodeInner, this.subgraph)
   }
 
-  // output(f: any): OutputNode {
-  //   return new OutputNode()
-  // }
+  union<O extends Array<any>, G extends Array<any>>(other: Node<O>): Node<G> {
+    const nodeInner = this.sugar.builder.addNode(
+      this.subgraph,
+      new this.sugar.dida.NodeSpec.Union([this.inner, other.inner]),
+    );
+
+    return new Node<G>(this.sugar, nodeInner, this.subgraph)
+  }
+
+  timestampPop(): Node<T> {
+    const nodeInner = this.sugar.builder.addNode(
+      this.subgraph,
+      new this.sugar.dida.NodeSpec.TimestampPop(this.inner),
+    );
+
+    return new Node<T>(this.sugar, nodeInner, this.subgraph)
+  }
+
+  output(): OutputNode<T> {
+    const nodeInner = this.sugar.builder.addNode(
+      this.subgraph,
+      new this.sugar.dida.NodeSpec.Output(this.inner),
+    );
+
+    return new OutputNode<T>(this.sugar, nodeInner, this.subgraph)
+  }
 }
 
-export class InputNode extends Node {
-
-  push() { }
-  flush() { }
-  advance() { }
-}
-
-export class OutputNode extends Node {
-  pop() { }
-}
-
-export class JoinedNode extends Node { }
-
-export class IndexedNode extends Node {
-  distinct(): this {
-    return this
+export class InputNode<T extends Array<any>> extends Node<T> {
+  push(v: T, timestamp: Array<number>, diff: number) {
+    this.sugar.shard.pushInput(
+      this.inner,
+      new this.sugar.dida.Change(v, timestamp, diff)
+    )
   }
-  join(): this {
-    return this
+  flush() {
+    this.sugar.shard.flushInput(this.inner)
+  }
+
+  advance(timestamp: Array<number>) {
+    this.sugar.shard.advanceInput(this.inner, timestamp)
   }
 }
 
-export class DistinctNode extends IndexedNode { }
-export class ReduceNode extends IndexedNode { }
+export class OutputNode<T extends Array<any>> extends Node<T> {
+  pop(): T | undefined {
+    return this.inner.pop()
+  }
 
-export class TimestampPushedNode extends Node { }
+  *[Symbol.iterator]() {
+    while (true) {
+      const change_batch = this.sugar.shard.popOutput(this.inner);
+      if (change_batch == undefined) break;
+      yield change_batch
+    }
+  }
+}
+
+export class JoinedNode<T extends Array<any>> extends Node<T> { }
+
+export class IndexedNode<T extends Array<any>> extends Node<T> {
+  map<G extends Array<any>>(f: (input: T) => G): IndexedNode<G> {
+    return this.mapInner(f) as IndexedNode<G>
+  }
+
+  distinct(): DistinctNode<T> {
+    const nodeInner = this.sugar.builder.addNode(
+      this.subgraph,
+      new this.sugar.dida.NodeSpec.Distinct(this.inner)
+    )
+
+    return new DistinctNode<T>(this.sugar, nodeInner, this.subgraph)
+  }
+
+  join<G extends Array<any>, O extends Array<any>>(
+    other: IndexedNode<G>,
+    joinFirstNCols: number,
+  ): IndexedNode<O> {
+    //TODO
+    const nodeInner = this.sugar.builder.addNode(
+      this.subgraph,
+      new this.sugar.dida.NodeSpec.Join([this.inner, other.inner], joinFirstNCols),
+    )
+
+    return new IndexedNode<O>(this.sugar, nodeInner, this.subgraph)
+  }
+}
+
+export class DistinctNode<T extends Array<any>> extends IndexedNode<T> { }
+export class ReduceNode<T extends Array<any>> extends IndexedNode<T> { }
+
+export class TimestampPushedNode<T extends Array<any>> extends Node<T> { }
+export class TimestampPoppedNode<T extends Array<any>> extends Node<T> { }
+
+export class TimestampIncrementedNode<T extends Array<any>> extends Node<T> {
+  // TODO fix type
+  fixpoint(future: IndexedNode<any[]>) {
+    const builder = this.sugar.builder
+    builder.connectLoop(future.inner, this.inner)
+  }
+}
 
 // type HasIndex<T: Node> = T extends IndexedNode ? true : T extends DistinctNode ? T exte
--- bindings/ts/sugar_test.ts
+++ bindings/ts/sugar_test.ts
@@ -4,23 +4,60 @@ import Dida from "../wasm/zig-out/lib/dida.mjs";
 import { Buffer } from "https://deno.land/std/io/mod.ts";
 import { Sugar, Subgraph, Node, OutputNode } from "./sugar.ts";
 
-Deno.test("sugar example", async () => {
+async function loadDida(): Promise<Dida> {
   const file = await Deno.open("../wasm/zig-out/lib/dida.wasm");
   const b = new Buffer();
   await b.readFrom(file);
-  console.log(typeof b, typeof b.bytes());
-  file.close()
 
   const abi = await Abi(b.bytes());
-  const dida = new Dida(abi);
+  return new Dida(abi);
+}
+
+Deno.test("sugar example", async () => {
+  const dida = await loadDida()
 
   const sugar = new Sugar(dida);
 
-  const edges = sugar.input()
+  const edges = sugar.input<[string, string]>()
   const loop = sugar.loop()
-  const edges1 = loop.importNode(edges)
 
-  // const edges = sugar.input()
+  const edges1 = loop.importNode<[string, string]>(edges)
+  const reach = loop.loopNode<[string, string]>()
+
+  reach.fixpoint(
+    reach
+      .index()
+      .join(
+        // Swap columns
+        edges1.map(v => [v[1], v[0]]
+        ).index(),
+        1
+      )
+      // Without the first key, and flip order again
+      .map(v => [v[2], v[1]])
+      .union(edges1)
+      .index()
+      .distinct()
+  )
+
+
+  const out = loop.exportNode(reach).output()
+
+  sugar.build()
+
+  edges.push(["a", "b"], [0], 1);
+  edges.push(["b", "c"], [0], 1);
+  edges.push(["b", "d"], [0], 1);
+  edges.push(["c", "a"], [0], 1);
+  edges.push(["b", "c"], [1], -1);
+  edges.flush()
+  edges.advance([1])
+
+  for (const v of out) {
+    console.log("reach_out");
+    console.log(v)
+  }
+
 
 });
 
